3f7636f3e852b6bd3b323f8b3bde7c70adfee2a1
musicrokr <darryl.leong90@gmail.com>
2020-04-15 21:06:09 +0800

    Commit Make Reservation
    Initial Make Reservation code, using sample values

---
+++ MakeReservation.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 20:42:33 2020
+
+@author: darry
+"""
+
+import tagui as t
+
+#Sample Values
+reservation_date = "16/05/2020"
+reservation_time = "1730"
+party_size = "2"
+restaurant_name ='Yujin Izakaya'
+first_name = 'John'
+last_name='Tan'
+email_address = 'darryl.leong90@gmail.com'
+phone_number = '98769876'
+    
+def make_reservation(reservation_date,reservation_time,party_size,restaurant_name,first_name,last_name,email_address,phone_number):
+    try:
+        #Convert User Defined Values to System Usable Values
+        reservation_day=reservation_date.split('/')[0]
+        reservation_month =reservation_date.split('/')[1]
+        reservation_month=int(reservation_month)-1
+        reservation_year =reservation_date.split('/')[2]
+        reservation_time_int=int(reservation_time)
+        start_time_hr= reservation_time[:2]
+        if reservation_time_int>1159:
+            if start_time_hr!="12":
+                start_time_hr=int(start_time_hr)-12
+            start_time_option = str(start_time_hr)+":"+reservation_time[2:4]+" pm"
+        else:
+            start_time_option = str(start_time_hr)+":"+reservation_time[2:4]+" am"
+            
+        #Booking Parameters
+        chope_url ='https://www.chope.co/singapore-restaurants'
+        t.init()
+        t.url(chope_url)
+        t.wait(5)
+        #Date Field
+        t.click(f"(//input[contains(@id,'date-field')])[1]")
+        t.click(f"//td[@data-handler='selectDay'and @data-month='{reservation_month}']/a[text()='{reservation_day}']")
+        #Time Field
+        t.select(f"//select[contains(@id,'time-field')]",start_time_option)
+        #Number of Diners Field
+        t.click(f"(//input[contains(@id,'diners-field')])[1]")
+        t.select(f"//select[contains(@id,'adults')]",party_size)
+        #Restaurant Field
+        t.click(f"(//select[contains(@id,'sel-restaurant')])[1]")
+        t.type('//input[@class="select2-search__field"]', restaurant_name)
+        t.click('//span[@class="icon icon-search"]')
+        t.wait(5)
+        
+        #Secondary Page to Confirm Timing
+        t.click(f"//a[@rname='{restaurant_name}' and text()='{start_time_option}']")
+        t.wait(5)
+        t.click(f"//input[@id='btn_sub' and @value='Book Now']")
+        t.wait(5)
+        
+        #Booking Confirmation
+        t.popup('https://book.chope.co/')
+        #First Name
+        t.type('//input[@id="forename"]',first_name)
+        #Last Name
+        t.type('//input[@id="surname"]',last_name)
+        #Email
+        t.type('//input[@id="email"]',email_address)
+        #Phone Number
+        t.type('//input[@id="telephone"]',phone_number)
+        #Agree Terms & Conditions
+        t.click(f"//input[@name='agree_term_conditions']")
+        #Confirm Booking
+        t.click(f"//button[@id='check_book_now']")
+        t.wait(5)
+        t.close()
+        print('Success')
+        
+    except:
+        print('Error')
+        
+        
+make_reservation(reservation_date,reservation_time,party_size,restaurant_name,first_name,last_name,email_address,phone_number)
