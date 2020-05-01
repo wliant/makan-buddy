56d4be6456991d0e0e2ab8c1960844ecdea1684d
Tan Wei Lian <twl_william89@hotmail.com>
2020-05-01 21:16:38 +0800

    save state to file

--- intelligence.py
+++ intelligence.py
@@ -14,7 +14,29 @@ class Intel:
         self.restaurants = self.load_restaurants()
         print("number of restaurants: {}", len(self.restaurants))
         #self.neighbors = self.load_neighbors()
-        self.queries = []
+        self.queries = self.load_queries()
+
+    def load_queries(self):
+        if os.path.isfile("queries.json"):
+            with open("queries.json", "r") as infile:
+                result = json.load(infile)
+            return result
+        else:
+            return []
+    def write_queries(self):
+        with open("queries.json", 'w') as outfile:
+            json.dump(self.queries, outfile)
+
+    def load_results(self):
+        if os.path.isfile("results.json"):
+            with open("results.json", 'r') as infile:
+                result = json.load(infile)
+            return result
+        else:
+            return []
+    def write_results(self, results):
+        with open("results.json", "w") as outfile:
+            json.dump(results, outfile)
     def load_restaurants(self):
         results = {}
         for f in os.listdir(restaurant_json_directory):
@@ -42,52 +64,54 @@ class Intel:
         rand_image = random.choice(list(rand_res[1]["images"].items()))
 
         self.queries.append({"npz": rand_image[1], "path": rand_image[0], "positive": 0, "neutral": 0, "negative": 0})
+        self.write_queries()
         return len(self.queries), rand_image[0]
 
     def update_response(self, image_id, positive, neutral, negative):
         self.queries[image_id-1]["positive"] = positive
         self.queries[image_id-1]["neutral"] = neutral
         self.queries[image_id-1]["negative"] = negative
+        self.write_queries()
+
 
     def get_result(self):
-        neighbors = self.load_neighbors()
-        occurrence = {}
-        data_files = set()
-        query_files = []
-        weights = []
-        for query in self.queries:
-            weights.append(query["positive"] / float(query["positive"] + query["neutral"] + query["negative"]))
-            query_files.append(query["npz"])
-            nn = neighbors[query["npz"]]
-            for i in nn:
-                data_files.add(i["filename"])
+        loaded_results = self.load_results()
+        if len(loaded_results) == 0:
+            neighbors = self.load_neighbors()
+            occurrence = {}
+            data_files = set()
+            query_files = []
+            weights = []
+            for query in self.queries:
+                w = (query["positive"] - query["negative"]) / float(10)
+                if w == 0:
+                    continue
+                weights.append(w)
+                query_files.append(query["npz"])
+                nn = neighbors[query["npz"]]
+                for i in nn:
+                    data_files.add(i["filename"])
 
-        mqknn = MQKNN(d, K, data_files, query_files, weights)
-        result = mqknn.get_result()
+            mqknn = MQKNN(d, K, data_files, query_files, weights)
+            result = mqknn.get_result()
 
-        results = []
-        for r in result:
-            restaurant_id = self.get_npz_restaurant(r)
-            restaurant_obj = self.restaurants[restaurant_id]
-            results.append((restaurant_obj["Name"], [i for i in restaurant_obj["images"].keys()]))
+            loaded_results = []
+            for r in result:
+                restaurant_id = self.get_npz_restaurant(r)
+                restaurant_obj = self.restaurants[restaurant_id]
+                loaded_results.append({"name": restaurant_obj["Name"], "images":[i for i in restaurant_obj["images"].keys()]})
+            self.queries = []
+            self.write_queries()
+        
+        returned_result = loaded_results[0]
+        loaded_results = loaded_results[1:]
+        self.write_results(loaded_results)
+        
+        return (returned_result["name"], returned_result["images"])
 
-        return results[0]
     def get_npz_restaurant(self, npz):
         st = npz.replace("npz/", "").replace(".npz", "")
         split = st.split("_")
         restaurant_id = "{}_{}".format(split[0], split[1])
         return restaurant_id
-        #return self.restaurants[restaurant_id]
 
-
-        
-
-#i = Intel()
-#print("init complete")
-#for j in range(0, 5):
-    #print(i.get_query())
-#    id,path = i.get_query()
-#    i.update_response(id, 3, 0, 0)
-
-#print("getting result")
-#print(i.get_result())
